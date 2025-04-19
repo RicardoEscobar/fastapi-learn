259d4fc996779c00ee628363c5c3c1aaaf4feb7a
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2025-04-19 16:12:28 -0600

    Add BookRequest class with validation fields

---
+++ bookrequest.py
@@ -0,0 +1,11 @@
+"""This module contains the BookRequest class."""
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(ge=1, le=5)
