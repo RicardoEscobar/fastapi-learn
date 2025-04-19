6031166c125c9c0267f1b2758ec6ae68a598dc0d
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2025-04-19 16:12:24 -0600

    Add Book class with initialization and attributes

---
+++ book.py
@@ -0,0 +1,27 @@
+"""This is a book module that contains the Book class."""
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(
+        self, id: int, title: str, author: str, description: str, rating: int
+    ) -> None:
+        """Initialize a Book instance with the given attributes.
+
+        Args:
+            id (int): The ID of the book.
+            title (str): The title of the book.
+            author (str): The author of the book.
+            description (str): A brief description of the book.
+            rating (int): The rating of the book.
+        """
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
