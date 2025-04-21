195db0f2693334839cb9b075ca25d525d1494492
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2025-04-21 11:13:35 -0600

    Refactor BookRequest model to include detailed field descriptions and add example schema

--- bookrequest.py
+++ bookrequest.py
@@ -1,11 +1,23 @@
 """This module contains the BookRequest class."""
+
 from typing import Optional
 from pydantic import BaseModel, Field
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="ID is not needed for create.", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(ge=1, le=5)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "codingwithroby",
+                "description": "A new description of a book.",
+                "rating": 5,
+            }
+        }
+    }
