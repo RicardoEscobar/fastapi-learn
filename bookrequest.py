c5b9b3c3f32a86195740a55897091b5d3fee8bac
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2025-04-22 20:25:44 -0600

    Make fields in BookRequest model optional and set default values

--- bookrequest.py
+++ bookrequest.py
@@ -6,10 +6,11 @@ from pydantic import BaseModel, Field
 
 class BookRequest(BaseModel):
     id: Optional[int] = Field(description="ID is not needed for create.", default=None)
-    title: str = Field(min_length=3)
-    author: str = Field(min_length=1)
-    description: str = Field(min_length=1, max_length=100)
-    rating: int = Field(ge=1, le=5)
+    title: Optional[str] = Field(min_length=3, default="Book title")
+    author: Optional[str] = Field(min_length=1, default="Book author")
+    description: Optional[str] = Field(min_length=1, max_length=100, default="Book description")
+    rating: Optional[int] = Field(ge=1, le=5, default=1)
+    published_date: Optional[int] = Field(gt=1999, lt=2031, description="Year of publication", default=2025)
 
     model_config = {
         "json_schema_extra": {
